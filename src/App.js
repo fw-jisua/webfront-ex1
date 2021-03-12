77ed640f5ee05f5a47784c90afb475b8b4bef444
fw-jisua <jisua@futurewalks.com>
2021-03-12 10:47:31 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,256 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {  
+  return (
+    <div className="whole">
+      <div className="w1">
+        <table className="table1" cellPadding="0" cellSpacing="0">
+          <tr>
+            <td>배송 상태 필터&nbsp;
+              <select className="selec-box">
+                <option value="1">모든 상태</option>
+                <option value="2">결제 완료</option>
+                <option value="3">배송중</option>
+                <option value="4">배송 완료</option>
+                <option value="5">취소 완료</option>
+                <option value="6">주문 확인</option>
+                <option value="7">취소 요청</option>
+              </select>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div className="w2">
+        <div className="table2">
+          <table className="t2-1" cellPadding="0" cellSpacing="0">
+            <tbody>
+              <tr>
+                <td rowSpan="4" className="list-img"><img src="https://cdn-dev.ssoldot.me/media/etc/1615348336603_eYWRXgXFJkciQnBePcUj.jpeg" className="img1" alt="Hoodie"></img></td>
+                <td className="list-title">상품명</td>
+                <td>[F&F Product] Footmark Heavyweight Bulky Hoodie 'AF Blue'</td>
+                <td className="list-title">주문자</td>
+                <td>퓨처웍스</td>
+                <td className="list-title">가격</td>
+                <td><span>98,000</span>원</td>
+              </tr>
+              <tr>
+                <td className="list-title">주소</td>
+                <td colSpan="3">"서울 강남구 논현로 149길 31 논현동"
+                    "4층 401호"
+                    "06040"
+                    " (베송메세지 : " 
+                    ")"
+                </td>
+                <td className="list-title">연락처</td>
+                <td>01000000000</td>
+              </tr>
+              <tr>
+                <td className="list-title">주문일자</td>
+                <td>2021년 03월 16일</td>
+                <td className="list-title">주문번호</td>
+                <td>7666202102010655388</td>
+                <td className="list-title">배송상태</td>
+                <td>취소완료</td>
+              </tr>
+              <tr>
+                <td className="list-title">송장번호</td>
+                <td colSpan="4">우체국</td>
+                <td><a href="#">
+                      <span>주문 상세 ▷</span>
+                    </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <br></br>
+          <div className="table2">
+            <table className="t2-1" cellPadding="0" cellSpacing="0">
+              <tbody>
+                <tr>
+                  <td rowSpan="4" className="list-img"><img src="https://cdn-dev.ssoldot.me/media/etc/1615348336603_eYWRXgXFJkciQnBePcUj.jpeg" className="img1" alt="Hoodie"></img></td>
+                  <td className="list-title">상품명</td>
+                  <td>[F&F Product] Footmark Heavyweight Bulky Hoodie 'AF Blue'</td>
+                  <td className="list-title">주문자</td>
+                  <td>퓨처웍스</td>
+                  <td className="list-title">가격</td>
+                  <td><span>98,000</span>원</td>
+                </tr>
+                <tr>
+                  <td className="list-title">주소</td>
+                  <td colSpan="3">"서울 강남구 논현로 149길 31 논현동"
+                      "4층 401호"
+                      "06040"
+                      " (베송메세지 : " 
+                      ")"
+                  </td>
+                  <td className="list-title">연락처</td>
+                  <td>01000000000</td>
+                </tr>
+                <tr>
+                  <td className="list-title">주문일자</td>
+                  <td>2021년 03월 16일</td>
+                  <td className="list-title">주문번호</td>
+                  <td>7666202102010655388</td>
+                  <td className="list-title">배송상태</td>
+                  <td>취소완료</td>
+                </tr>
+                <tr>
+                  <td className="list-title">송장번호</td>
+                  <td colSpan="4">우체국</td>
+                  <td><a href="#">
+                        <span>주문 상세 ▷</span>
+                      </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <br></br>
+          <div className="table2">
+            <table className="t2-1" cellPadding="0" cellSpacing="0">
+              <tbody>
+                <tr>
+                  <td rowSpan="4" className="list-img"><img src="https://cdn-dev.ssoldot.me/media/etc/1615348336603_eYWRXgXFJkciQnBePcUj.jpeg" className="img1" alt="Hoodie"></img></td>
+                  <td className="list-title">상품명</td>
+                  <td>[F&F Product] Footmark Heavyweight Bulky Hoodie 'AF Blue'</td>
+                  <td className="list-title">주문자</td>
+                  <td>퓨처웍스</td>
+                  <td className="list-title">가격</td>
+                  <td><span>98,000</span>원</td>
+                </tr>
+                <tr>
+                  <td className="list-title">주소</td>
+                  <td colSpan="3">"서울 강남구 논현로 149길 31 논현동"
+                      "4층 401호"
+                      "06040"
+                      " (베송메세지 : " 
+                      ")"
+                  </td>
+                  <td className="list-title">연락처</td>
+                  <td>01000000000</td>
+                </tr>
+                <tr>
+                  <td className="list-title">주문일자</td>
+                  <td>2021년 03월 16일</td>
+                  <td className="list-title">주문번호</td>
+                  <td>7666202102010655388</td>
+                  <td className="list-title">배송상태</td>
+                  <td>취소완료</td>
+                </tr>
+                <tr>
+                  <td className="list-title">송장번호</td>
+                  <td colSpan="4">우체국</td>
+                  <td><a href="#">
+                        <span>주문 상세 ▷</span>
+                      </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <br></br>
+          <div className="table2">
+            <table className="t2-1" cellPadding="0" cellSpacing="0">
+              <tbody>
+                <tr>
+                  <td rowSpan="4" className="list-img"><img src="https://cdn-dev.ssoldot.me/media/etc/1615348336603_eYWRXgXFJkciQnBePcUj.jpeg" className="img1" alt="Hoodie"></img></td>
+                  <td className="list-title">상품명</td>
+                  <td>[F&F Product] Footmark Heavyweight Bulky Hoodie 'AF Blue'</td>
+                  <td className="list-title">주문자</td>
+                  <td>퓨처웍스</td>
+                  <td className="list-title">가격</td>
+                  <td><span>98,000</span>원</td>
+                </tr>
+                <tr>
+                  <td className="list-title">주소</td>
+                  <td colSpan="3">"서울 강남구 논현로 149길 31 논현동"
+                      "4층 401호"
+                      "06040"
+                      " (베송메세지 : " 
+                      ")"
+                  </td>
+                  <td className="list-title">연락처</td>
+                  <td>01000000000</td>
+                </tr>
+                <tr>
+                  <td className="list-title">주문일자</td>
+                  <td>2021년 03월 16일</td>
+                  <td className="list-title">주문번호</td>
+                  <td>7666202102010655388</td>
+                  <td className="list-title">배송상태</td>
+                  <td>취소완료</td>
+                </tr>
+                <tr>
+                  <td className="list-title">송장번호</td>
+                  <td colSpan="4">우체국</td>
+                  <td><a href="#">
+                        <span>주문 상세 ▷</span>
+                      </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <br></br>
+          <div className="table2">
+            <table className="t2-1" cellPadding="0" cellSpacing="0">
+              <tbody>
+                <tr>
+                  <td rowSpan="4" className="list-img"><img src="https://cdn-dev.ssoldot.me/media/etc/1615348336603_eYWRXgXFJkciQnBePcUj.jpeg" className="img1" alt="Hoodie"></img></td>
+                  <td className="list-title">상품명</td>
+                  <td>[F&F Product] Footmark Heavyweight Bulky Hoodie 'AF Blue'</td>
+                  <td className="list-title">주문자</td>
+                  <td>퓨처웍스</td>
+                  <td className="list-title">가격</td>
+                  <td><span>98,000</span>원</td>
+                </tr>
+                <tr>
+                  <td className="list-title">주소</td>
+                  <td colSpan="3">"서울 강남구 논현로 149길 31 논현동"
+                      "4층 401호"
+                      "06040"
+                      " (베송메세지 : " 
+                      ")"
+                  </td>
+                  <td className="list-title">연락처</td>
+                  <td>01000000000</td>
+                </tr>
+                <tr>
+                  <td className="list-title">주문일자</td>
+                  <td>2021년 03월 16일</td>
+                  <td className="list-title">주문번호</td>
+                  <td>7666202102010655388</td>
+                  <td className="list-title">배송상태</td>
+                  <td>취소완료</td>
+                </tr>
+                <tr>
+                  <td className="list-title">송장번호</td>
+                  <td colSpan="4">우체국</td>
+                  <td><a href="#">
+                        <span>주문 상세 ▷</span>
+                      </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div className="w3">
+          <div className="btn-list">
+            <button className="btn">1</button>
+            <button className="btn">2</button>
+            <button className="btn">3</button>
+            <button className="btn">4</button>
+            <button className="btn">5</button>
+          </div>
+      </div>
+    </div>
+   
+
+    
+  );
+}
+
+export default App;
